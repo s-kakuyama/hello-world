f7214255d4e9fe1a833766061f22afb9f79ec898
s-kakuyama <kaku8ma@gmail.com>
2024-04-29 14:42:28 +0900

    Initial commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <div>
+    <HelloWorld msg="Hello, World!" />
+  </div>
+</template>
+
+<style scoped></style>
